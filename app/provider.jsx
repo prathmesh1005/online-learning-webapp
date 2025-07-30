5a34b8de521ad5f9ea4e82c6ddf6a173216a8ae6
Prakhar504 <prakharjain504@gmail.com>
2025-07-30 12:29:42 +0530

    provider

---
+++ app/provider.jsx
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react'
+
+const provider = ({children}) => {
+
+    const {user} = usserUser();
+    const CreateNewUser=async()=>{
+        const result = await axios.post('/api/user',{
+            name:user?.fullname,
+            email:user?.primaryEmailAddress?.emailAddress
+        });
+        console.log(result.data);
+    }
+
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default provider
