5aa64f22908e238929942769a74739c257526b39
prathmesh1005 <enprathmeshpatil04@gmail.com>
2025-08-01 13:10:07 +0530

    Fully Completer user authentication and login

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
-import { Button } from "@/components/ui/button";
+import React from "react";
 import Image from "next/image";
+// import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button"; // adjust based on depth
+import { UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello World</h2>
-      <Button>Hello</Button>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-6 bg-gray-100">
+      <h1 className="text-3xl font-bold">Welcome to StudyXpert</h1>
+      <Button variant="default" size="lg">
+        Get Started
+      </Button>
+      <UserButton/>
+    </main>
   );
 }
